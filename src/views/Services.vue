f087165fa00f1305765335bdc5cda2aca6953cb3
jlongakit <jesterlongakit03@gmail.com>
2022-10-04 18:37:58 +0800

    Initial push | Native responsive css for mobile, tablet and pc

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,5 +1,296 @@
 <template>
   <div class="service">
-    <h1>This is an service page</h1>
+    <div class="imageHolder">
+      <img class="img" src="../assets/images/Services.svg">
+    </div>
+    <div class="pageTitle pt-4">
+      <h1><strong>SERVICES</strong></h1>
+    </div>
+    <!-- FIRST ROW ! -->
+    <div class="row" style="padding-top: 7%;">
+      <div class="column">
+        <img src="../assets/images/Servicesimg1.svg" class="columnImage">
+        <div class="cellText">
+          <h4>Software Development</h4>
+          <br>
+          <p>To design, develop, implement, </p>
+          <p>manage software development</p>
+          <p class="pb-5">and provide quality assurance</p>
+        </div>
+      </div>
+      <div class="column">
+        <img src="../assets/images/Servicesimg2.svg" class="columnImage">
+        <div class="cellText">
+          <h4>Manual Testing</h4>
+          <br>
+          <p>Manual functional testing of </p>
+          <p>system to ensure all features</p>
+          <p class="pb-5">work as expected</p>
+        </div>
+      </div>
+      <div class="column">
+        <img src="../assets/images/Servicesimg3.svg" class="columnImage">
+        <div class="cellText">
+          <h4>Automation Testing</h4>
+          <br>
+          <p>Automate all possible manual</p>
+          <p>functional testing</p>
+          <br>
+          <p>Tackle basic security testing and </p>
+          <p class="pb-5">we also provide API load testing</p>
+          <br>
+        </div>
+      </div>
+    </div>
+    <!-- SECOND ROW ! -->
+    <div class="row pt-5">
+      <div class="column">
+        <img src="../assets/images/Servicesimg4.svg" class="columnImage">
+        <div class="cellText">
+          <h4>Technical Support</h4>
+          <br>
+          <p>Support and maintain system, </p>
+          <p>install, and configures and solves</p>
+          <p class="pb-5">technical issues as they arise</p>
+        </div>
+      </div>
+      <div class="column">
+        <img src="../assets/images/Servicesimg5.svg" class="columnImage">
+        <div class="cellText">
+          <h4>System Maintenance</h4>
+          <br>
+          <p>Fixes problems related to</p>
+          <p>the software and hardware </p>
+          <p class="pb-5">components of computers</p>
+        </div>
+      </div>
+      <div class="column">
+        <img src="../assets/images/Servicesimg6.svg" class="columnImage">
+        <div class="cellText">
+          <h4>Computer Installation</h4>
+          <br>
+          <p>Setting up hardware and</p>
+          <p>installing, configuring software </p>
+          <p class="pb-5">and computer drivers</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+  .pageTitle{
+    font-size: 30px;
+    line-height: 35px;
+    letter-spacing: 0.3em;
+    color: #111111;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  .service{
+    width: 100%;
+  }
+
+  .img{
+    object-fit: cover;
+    /* object-position: -10% 45%; */
+    height: 100%;
+    width: 100%;
+  }
+
+  .imageHolder{
+    display: none;
+  }
+
+  .cellText{
+    text-align: left;
+    text-indent: 10%;
+    font-size: 10px;
+    line-height: 15px;
+    letter-spacing: 0.1em;
+    color: #111111;
+  }
+
+  .columnImage{
+    width: 100%;
+    padding-bottom: 15px;
+  }
+
+  @media only screen and (min-width: 768px){
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: 'Poppins', sans-serif;
+    }
+
+    .pageTitle{
+      display: none;
+    }
+
+    .imageHolder{
+    display: block;
+    height: 100%;
+    widows: 100%;
+    }
+    .cellText{
+    text-align: left;
+    text-indent: 25%;
+    font-size: 15px;
+    line-height: 25px;
+    letter-spacing: 0.3em;
+    color: #111111;
+    }
+    .columnImage{
+    width: 50%;
+    padding-bottom: 35px;
+    }
+  }
+  @media only screen and (min-width: 1000px){
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: 'Poppins', sans-serif;
+    }
+  
+    .img{
+      object-fit: cover;
+      /* object-position: -10% 45%; */
+      height: 80%;
+      width: 100%;
+    }
+
+    .pageTitle{
+      display: none;
+    }
+
+    .imageHolder{
+      background-color: green;
+      height: auto;
+    }
+
+    .row{
+      display: table;
+      width: 100%; /*Optional*/
+      table-layout: fixed; /*Optional*/
+      padding-left: 5%;
+      /* padding-top: 5%; */
+      border-spacing: 60px 0px;
+    }
+
+    .column{
+      display: table-cell;
+      /* background-color: red; */
+      border-style: solid;
+      border-width: 1px;
+      text-align: center;
+      margin: 0;
+      padding: 0;
+    }
+
+    .cellText{
+      text-align: left;
+      text-indent: 20px;
+      font-size: 10px;
+      line-height: 15px;
+      letter-spacing: 0.1em;
+      color: #111111;
+    }
+    .cellText h4{
+      text-align: left;
+      text-indent: 20px;
+      font-size: 15px;
+      line-height: 35px;
+      letter-spacing: 0.1em;
+      color: #111111;
+    }
+
+    .columnImage{
+      width: 100%;
+      padding-bottom: 20px;
+    }
+    }
+  @media only screen and (min-width: 2000px){
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+  
+  .img{
+    object-fit: cover;
+    /* object-position: -10% 45%; */
+    height: 100%;
+    width: 100%;
+  }
+
+  .pageTitle{
+    display: none;
+  }
+
+  .imageHolder{
+    background-color: green;
+    height: 500px;
+  }
+
+  .row{
+    display: table;
+    width: 85%; /*Optional*/
+    table-layout: fixed; /*Optional*/
+    padding-left: 15%;
+    /* padding-top: 5%; */
+    border-spacing: 60px 0px;
+  }
+
+  .column{
+    display: table-cell;
+    /* background-color: red; */
+    border-style: solid;
+    border-width: 1px;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+  }
+
+  .cellText{
+    text-align: left;
+    text-indent: 15%;
+    font-size: 15px;
+    line-height: 25px;
+    letter-spacing: 0.1em;
+    color: #111111;
+  }
+
+  .cellText h4{
+    text-align: left;
+    text-indent: 15%;
+    font-size: 20px;
+    line-height: 25px;
+    letter-spacing: 0.1em;
+    color: #111111;
+  }
+
+  .columnImage{
+    width: 100%;
+    padding-bottom: 70px;
+  }
+  }
+
+</style>
+
